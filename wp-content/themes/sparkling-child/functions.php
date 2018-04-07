3bfe70925b43bc2ef4999edc688f5b13a05326c9
Thomas Reeves <treeves@northislandcollege.ca>
2018-04-07 15:25:30 -0700

    adds function to hide title on front page

--- wp-content/themes/sparkling-child/functions.php
+++ wp-content/themes/sparkling-child/functions.php
@@ -3,4 +3,21 @@ add_action( 'wp_enqueue_scripts', 'enqueue_child_theme_styles', PHP_INT_MAX);
 function enqueue_child_theme_styles() {
   wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
+
+function ala_hidetitle_class($classes) {
+
+  if (  is_front_page() ) :
+  
+  $classes[] = 'hidetitle';
+  
+  return $classes;
+  
+  endif;
+  
+  return $classes;
+  
+  }
+  
+  add_filter('post_class', 'ala_hidetitle_class');
+
 ?>
