d53900f904fb867d4d0bb6694770a01dc2dfb8ce
Thomas Reeves <treeves@northislandcollege.ca>
2018-04-12 22:20:38 -0700

    adds conditional if front page home-logo goes to contact screen

--- wp-content/themes/sparkling-child/header.php
+++ wp-content/themes/sparkling-child/header.php
@@ -49,8 +49,12 @@ if ( isset( $_SERVER['HTTP_USER_AGENT'] ) && (strpos( $_SERVER['HTTP_USER_AGENT'
 
 														<div id="logo">
 															<?php if ( get_header_image() != '' ) { ?>
-																	<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php header_image(); ?>"  height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="<?php bloginfo( 'name' ); ?>"/></a>
-																		<?php if ( is_home() ) { ?>
+																<?php if ( is_front_page()) { ?>
+																	<a href="<?php echo esc_url( home_url( '/contact' ) ); ?>"><img src="<?php header_image(); ?>"  height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="<?php bloginfo( 'name' ); ?>"/></a>
+																	<?php } else { ?>
+																		<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php header_image(); ?>"  height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="<?php bloginfo( 'name' ); ?>"/></a>
+																	<?php } ?>
+																		<?php if ( is_home()) { ?>
 																		<h1 class="site-name hide-site-name"><a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 																	<?php }
 } else {
